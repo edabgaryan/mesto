387e727eeba24bba6cac9fc9b74f6d927ebe0d28
Edgar <101365753+Edgar0995@users.noreply.github.com>
2022-07-04 18:36:01 +0200

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,37 @@
+let userName = "Жак-Ив Кусто"
+let userAbout = "Исследователь океана"
+document.getElementById("userAbout").innerHTML = userAbout
+function aboutinputOnchange() {
+    userAbout = document.getElementById("userAbout-input").value;
+}
+document.getElementById("userName").innerHTML = userName 
+function inputOnchange() {
+    userName = document.getElementById("userName-input").value;
+}
+const savebtn = document.getElementById("savebtn");
+const modal = document.getElementById("myPopup");
+const btn = document.getElementById("myBtn");
+const span = document.getElementsByClassName("popup__close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+    document.getElementById("userName").innerHTML = userName 
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+    modal.style.display = "none";
+    }
+}
+savebtn.onclick = function() {
+    modal.style.display = "none";
+    document.getElementById("userName").innerHTML = userName 
+    document.getElementById("userAbout").innerHTML = userAbout
+}
+function like (itemId) {
+    const likeBtn = document.getElementById(`likebtn${itemId}`);
+    if (likeBtn) {
+        likeBtn.classList.add("card__like-active"); 
+    }
+}
