b1e44e43a53163ce62aa66303a0a9b22214e1640
Edgar <101365753+Edgar0995@users.noreply.github.com>
2022-07-07 13:43:53 +0200

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,51 @@
+
+let userName = "Жак-Ив Кусто"
+let userAbout = "Исследователь океана"
+document.getElementById("userAbout").innerHTML = userAbout
+function aboutinputOnchange() {
+    userAbout = document.getElementById("userAbout-input").value;
+}
+document.getElementById("userName").innerHTML = userName 
+function inputOnchange() {
+    userName = document.getElementById("userName-input").value;
+}
+const savebtn = document.getElementById("savebtn");
+const modal = document.getElementById("myPopup");
+const btn = document.getElementById("myBtn");
+const span = document.getElementsByClassName("popup__close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+    modal.style.display = "none";
+    }
+}
+savebtn.onclick = function() {
+    modal.style.display = "none";
+    document.getElementById("userName").innerHTML = userName 
+    document.getElementById("userAbout").innerHTML = userAbout
+}
+input = document.getElementById("userName-input");
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter"){
+    event.preventDefault();
+    document.getElementById("savebtn").click();
+    }
+    })
+input = document.getElementById("userAbout-input");
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("savebtn").click();
+    }
+    });
+function like (itemId) {
+    const likeBtn = document.getElementById(`likebtn${itemId}`);
+    if (likeBtn) {
+        likeBtn.classList.add("card__like_active"); 
+    }
+}
