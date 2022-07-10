06875eaa03a362dd56dfd385491d9167bc51a261
Edgar <101365753+Edgar0995@users.noreply.github.com>
2022-07-11 01:03:28 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,30 @@
-let userName = "Жак-Ив Кусто"
-let userAbout = "Исследователь океана"
-document.querySelector(".profile__info-paragraph").innerHTML = userAbout
-function aboutinputOnchange() {
-    userAbout = document.querySelector(".profile__info-paragraph").value;
-}
-document.querySelector(".profile__info-title").innerHTML = userName 
-function inputOnchange() {
-    userName = document.querySelector(".profile__info-title").value;
-}
-const savebtn = document.querySelector(".popup__form-btn");
-const modal = document.querySelector(".popup");
-const btn = document.querySelector(".profile__edit-button");
-const span = document.querySelector(".popup__close");
+let popupContainer = document.querySelector('.popup');
+let addButtonPopup = document.querySelector('.profile__edit-button');
+let closeButtonPopup = document.querySelector('.popup__close');
+let form = document.querySelector('.popup__form');
+let fullNameProfile = document.querySelector('.profile__info-title');
+let professionProfile = document.querySelector('.profile__info-paragraph');
+let fullNameForm = document.querySelector('.popup__input_form_name');
+let professionForm = document.querySelector('.popup__input_form_profession');
 
+let popupOpane = function() {
+    popupContainer.classList.add('popup_opened');
+    fullNameForm.value = fullNameProfile.textContent;
+    professionForm.value = professionProfile.textContent;
+};
 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-span.onclick = function() {
-    modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-    modal.style.display = "none";
-    }
+let popupClose = function() {
+    popupContainer.classList.remove('popup_opened');
 }
 
-savebtn.onclick = function() {
-    modal.style.display = "none";
-    document.querySelector(".profile__info-title").innerHTML = userName 
-    document.querySelector(".profile__info-paragraph").innerHTML = userAbout
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    fullNameProfile.textContent = fullNameForm.value;
+    professionProfile.textContent = professionForm.value;
+    popupClose();
 }
-input = document.querySelector(".popup__form-input");
-input.addEventListener("keypress", function(event) {
-    if (event.key === "Enter"){
-    event.preventDefault();
-    document.querySelector(".popup__form-btn").click();
-    }
-    })
-input = document.querySelector(".popup__form-input");
-input.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-    event.preventDefault();
-    document.querySelector(".popup__form-btn").click();
-    }
-    });
+
+addButtonPopup.addEventListener('click', () => popupOpane ());
+closeButtonPopup.addEventListener('click', () => popupClose());
+
+form.addEventListener('submit', formSubmitHandler);
