6050fcbd1b0ba8813e827af27162179e48ea240e
Edgar <101365753+Edgar0995@users.noreply.github.com>
2022-09-06 12:46:01 +0200

    П.Р. 8

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,43 +1,31 @@
-import { Popup } from "./Popup.js";
+import Popup from './Popup.js';
 
 export default class PopupWithForm extends Popup {
-  constructor(popupSelector, submitFormForPopup) {
-    super(popupSelector);
-    this._submitFormForPopup = submitFormForPopup;
-
-    this.form = this._popup.querySelectorAll('.popup__form');
-    this._inputList = this.form.querySelectorAll('.popup__input');
+  constructor(selectorPopup, {submitForm}, selectors) {
+    super(selectorPopup, selectors);
+    this._submitForm = submitForm;
+    this._popupForm = this._popup.querySelector(selectors.popupForm); ////All ?
+    this._inputArray = this._popup.querySelectorAll(selectors.formInput);
   }
 
-  // закрытие поп-апа
   close() {
     super.close();
-    this.form.reset();
+    this._popupForm.reset();
   }
 
-  setInputValues(data) {
-    this._inputList.forEach((input) => {
-      input.value = data[input.name];
+  _getInputValues() {
+    this._valueObject = {};
+    this._inputArray.forEach(item => {
+      this._valueObject[item.name] = item.value;
     });
+    return this._valueObject;
   }
 
-  // возвращает текст с inputs
-  getInputValues() {
-    // создаём пустой объект
-    this._formValues = {};
-
-    // добавляем в этот объект значения всех полей
-    this._inputList.forEach(input => {
-      this._formValues[input.name] = input.value;
-    });
-
-    // возвращаем объект значений
-    return this._formValues;
-  }
-
-  //слушатели
   setEventListeners() {
     super.setEventListeners();
-    this.form.addEventListener('submit', this._submitFormForPopup);
+    this._popupForm.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      this._submitForm(this._getInputValues());
+    });
   }
 }
