308386b6560f6f8804c0aacc2c274b8de13ea653
Edgar <101365753+Edgar0995@users.noreply.github.com>
2022-09-06 12:47:23 +0200

    П.Р. 8

--- src/components/section.js
+++ src/components/section.js
@@ -1,18 +1,17 @@
 export default class Section {
-    constructor({ items, renderer }, containerSelector) {
-      this._initialArray = items;
-      this._renderer = renderer;
-      this._container = document.querySelector(containerSelector);
-      this.addItem = this.addItem.bind(this);
-    }
-
-    addItem(element) {
-      this._container.prepend(element);
-    }
-  
-    renderItems() {
-      this._initialArray.forEach(item => {
-        this._renderer(item);
-      });
-    }
+  constructor({items, renderer}, elementsContainer) {
+    this._initialArray = items;
+    this._renderer = renderer;
+    this._container = elementsContainer;
   }
+
+  renderItems() {
+    this._initialArray.forEach((item) => {
+      this._renderer(item);
+    });    
+  }
+
+  addItem(element) {
+    this._container.prepend(element);
+  }
+}
