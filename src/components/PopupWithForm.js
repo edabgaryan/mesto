2fcd2ee2b880897df44ef804795a4c851c161fb3
Edgar <101365753+Edgar0995@users.noreply.github.com>
2022-09-08 15:45:45 +0200

    Update PopupWithForm.js

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,7 +4,7 @@ export default class PopupWithForm extends Popup {
   constructor(selectorPopup, {submitForm}, selectors) {
     super(selectorPopup, selectors);
     this._submitForm = submitForm;
-    this._popupForm = this._popup.querySelector(selectors.popupForm); ////All ?
+    this._popupForm = this._popup.querySelector(selectors.popupForm);
     this._inputArray = this._popup.querySelectorAll(selectors.formInput);
   }
 
@@ -14,11 +14,11 @@ export default class PopupWithForm extends Popup {
   }
 
   _getInputValues() {
-    this._valueObject = {};
+    this._inputsValue = {};
     this._inputArray.forEach(item => {
-      this._valueObject[item.name] = item.value;
+      this._inputsValue[item.name] = item.value;
     });
-    return this._valueObject;
+    return this._inputsValue;
   }
 
   setEventListeners() {
