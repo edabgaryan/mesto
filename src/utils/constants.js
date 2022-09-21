b90f6fd72a66fcc4cdf9a04dd905b0e32bd6cfa7
Edgar <101365753+Edgar0995@users.noreply.github.com>
2022-09-21 19:42:10 +0200

    пр 9

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,38 +1,15 @@
-export const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
-
 export const selectors = {
     //Popup
-    popupFormProfile: ".popup_form_profile",
-    popupFormAdd: ".popup_form_add",
-    popupOpenPicture: ".popup_open_picture",
-    closeButtonPopup: ".popup__close",
-    popupFormName: ".popup__form-name", 
-    popupFormPlace: ".popup__form-place",
+    popupContainerFormProfile: ".popup_form_profile",
+    popupContainerFormNewPlace: ".popup_form_add",
+    popupContainerOpenPicture: ".popup_open_picture",
+    popupContainerDeletePicture: ".popup_delete-picture",
+    buttonClosePopup: ".popup__close",
+    nameProfileFormPopup: ".popup__form-name", 
+    newPlaceFormPopup: ".popup__form-place",
+    avatarFormProfile: ".popup__form_avatar",
+    buttonSave: '.popup__form-btn',
+    popupAvatar: ".popup_edit-avatar",
     fullNameForm: ".popup__input_form_name",
     professionForm: ".popup__input_form_profession",
     popupPicture: ".popup__picture",
@@ -40,37 +17,32 @@ export const selectors = {
     namePlaceFormPopup: ".popup__input_form_name-place",
     placeSourceFormPopup: ".popup__input_form_source-on-place",
     //Profile
-    infoButtonProfile: ".profile__info-button",
-    addButtonProfile: ".profile__add-button",
+    buttonEditProfile: ".profile__info-button",
+    buttonAddPlace: ".profile__add-button",
     fullNameProfile: ".profile__title",
     professionProfile: ".profile__paragraph",
+    avatarProfile: ".profile__ahutor",
     //Card
     elementsContainer: ".elements__cards",
     card: ".card",
-    template: ".template",
-    trashButton: ".card__button-delete",
-    cardButtonLike: ".card__like",
+    templateCard: ".template",
+    buttonDeleteCard: ".card__button-delete",
+    buttonCardLike: ".card__like",
     cardImage: ".card__image",
     cardTitle: ".card__title",
     popupForm: '.popup__form',
     formInput: '.popup__input',
   };
 
-export const validationConfig = {
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__form-btn',
-    inactiveButtonClass: 'popup__form-btn_disabled',
-    buttonWithFormValidity: 'popup__form-btn_hover',
-    inputErrorClass: 'popup__input_border-bottom_red',
-    inputValidity: 'popup__input_border-bottom_black',
-    errorVisibility: 'popup__input-text_error-visible',
-};  
+export const buttonEditProfile = document.querySelector(selectors.buttonEditProfile);
+export const buttonAddPlace = document.querySelector(selectors.buttonAddPlace);
+export const editAvatarProfile = document.querySelector(selectors.avatarProfile);
 
-export const infoButtonProfile = document.querySelector(selectors.infoButtonProfile);
-export const addButtonProfile = document.querySelector(selectors.addButtonProfile);
-export const popupFormName = document.querySelector(selectors.popupFormName);
-export const popupFormPlace = document.querySelector(selectors.popupFormPlace);
-export const fullNameForm = document.querySelector(selectors.fullNameForm);
-export const professionForm = document.querySelector(selectors.professionForm);
+export const nameProfileFormPopup = document.querySelector(selectors.nameProfileFormPopup);
+export const newPlaceFormPopup = document.querySelector(selectors.newPlaceFormPopup);
+export const avatarFormPopup = document.querySelector(selectors.avatarFormProfile);
+
+export const fullNameInputFormPopup = document.querySelector(selectors.fullNameForm);
+export const professionInputFormPopup = document.querySelector(selectors.professionForm);
+
 export const elementsContainer = document.querySelector(selectors.elementsContainer);
